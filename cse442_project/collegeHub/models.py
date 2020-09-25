867cbf408c9b225331b42b0570c40f46534cc23b
Mayank Jha <mayank@Mayanks-MacBook-Pro-2.local>
2020-09-25 07:04:46 +0530

    Created user model [ issue #30]

--- cse442_project/collegeHub/models.py
+++ cse442_project/collegeHub/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from django.contrib.auth import models as auth_models
 # Create your models here.
+
+CurrentUser = get_user_model()
+
+class User(auth_models.User, auth_models.PermissionsMixin):
+
+    def __str__(self):
+        return "@{}".format(self.username)
