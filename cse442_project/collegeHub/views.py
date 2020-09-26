2fe64c6880788d6dda57978d4984fd3c51328508
rayanray <rayanray@buffalo.edu>
2020-09-26 23:24:51 +0530

    Personalized register page

--- cse442_project/collegeHub/views.py
+++ cse442_project/collegeHub/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import initRegisterForm
 
 # Create your views here.
 def register(user_request):
     if user_request.method == 'POST':
-        form = UserCreationForm(user_request.POST)
+        form = initRegisterForm(user_request.POST)
         if form.is_valid():
             form.save()
-            email = form.cleaned_data.get('email')
-            messages.success(user_request, f'Account created for {email}')
+            username = form.cleaned_data.get('username')
+            messages.success(user_request, f'Account created for {username}')
         else:
-            form = UserCreationForm()
+            form = initRegisterForm()
     return render(user_request, 'User/register.html',{'form':form})
     
 
