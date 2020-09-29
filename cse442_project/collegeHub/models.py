c519e11517c6ab2c0db7f49e21a46b412cf9a333
Mayank Jha <mayank@Mayanks-MacBook-Pro-2.local>
2020-09-30 01:47:52 +0530

    Integrated User profile into this branch. Fixed relation between experience and user profile

--- cse442_project/collegeHub/models.py
+++ cse442_project/collegeHub/models.py
@@ -12,9 +12,28 @@ class User(auth_models.User, auth_models.PermissionsMixin):
     def __str__(self):
         return "@{}".format(self.username)
 
+class UserProfile(models.Model):
+    profile_pic = models.ImageField(upload_to="images/", blank=True, null=True, default="images/BlueHead.jpg")
+    user = models.OneToOneField(CurrentUser, unique=True, on_delete=models.CASCADE, null=True)
+    bio = models.TextField(default="", blank=True)
+    bio_html = models.TextField(editable=False)
+
+    location = models.CharField(default="", blank=True, max_length=255)
+    occupation = models.CharField(default="", blank=True, max_length=255)
+    github = models.CharField(default="", blank=True, max_length=255)
+    instagram = models.CharField(default="", blank=True, max_length=255)
+    linkedin = models.CharField(default="", blank=True, max_length=255)
+
+    quote = models.CharField(default="", blank=True, max_length=255)
+
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        # return self.bio
+        return self.user.username
 
 class Experiences(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True, null=True, related_name='experience')
+    user = models.OneToOneField(CurrentUser, on_delete=models.CASCADE, unique=True, null=True, related_name='experience')
 
 
 class Section(models.Model):
