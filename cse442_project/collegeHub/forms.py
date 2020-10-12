81eb0f9a82cdf2a0f95e6022238c971755944ab1
rayanray <rayanray@buffalo.edu>
2020-10-12 22:06:00 +0530

    local save

--- cse442_project/collegeHub/forms.py
+++ cse442_project/collegeHub/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
- class initRegisterForm(UserCreationForm):
+class initRegisterForm(UserCreationForm):
      email = forms.EmailField(required = True)
 
      class Meta:
