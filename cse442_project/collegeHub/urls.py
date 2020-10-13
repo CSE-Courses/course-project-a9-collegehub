c251892bb1e23c02e44c54637d30b2f07e56560d
cjperez2017 <43019316+cjperez2017@users.noreply.github.com>
2020-10-13 14:58:43 -0500

    Update urls.py

--- cse442_project/collegeHub/urls.py
+++ cse442_project/collegeHub/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     path('emailSent/', views.register_email_sent.as_view(), name="emailed"),
     path('emailConfirmed/', views.register_confirmed.as_view(), name="confirmed"),
     path('emaiNotConfirmed/', views.register_not_confirmed.as_view(), name="not_confirmed"),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate')
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('login/', auth_views.LoginView.as_view('templates/login.html'), name='login'),
+    path('logout/', auth_views.Logout.as_view('templates/logout.html'), name='logout'),
+    path('profile/', auth_views.profile.as_view(), name='profile'),
+
 ]
