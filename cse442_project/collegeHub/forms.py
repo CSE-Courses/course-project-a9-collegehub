82ce60d32f0ae7cae2a8d5ebdb472fea22bf4923
Chris <cjperez2017@gmail.com>
2020-10-13 03:14:01 -0500

    Issue #56, #57, #60. Created the forms for a section, made the model for an education, made the form for an education, updated the test.html file to include all forms, also changed the experience model to be connected in user profile instead of user

--- cse442_project/collegeHub/forms.py
+++ cse442_project/collegeHub/forms.py
@@ -1,8 +1,20 @@
 from django import forms
-from .models import Specific
+from .models import Specific, Section, Education
+
+
+class SectionFrom(forms.ModelForm):
+    class Meta:
+        model = Section
+        fields = ('name', 'experiences')
 
 
 class SpecificForm(forms.ModelForm):
     class Meta:
         model = Specific
         fields = ('image', 'description', 'bullet_section', 'section')
+
+
+class EducationFrom(forms.ModelForm):
+    class Meta:
+        model = Education
+        fields = ('image', 'location', 'certification_name', 'description', 'month', 'year', 'profile')
--- cse442_project/collegeHub/views.py
+++ cse442_project/collegeHub/views.py
@@ -1,4 +1,6 @@
 import json
+
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
@@ -6,7 +8,7 @@ from django.contrib import messages
 from django.views.generic import TemplateView
 from django.urls import reverse, reverse_lazy
 from collegeHub import models
-from .forms import SpecificForm
+from .forms import SpecificForm, SectionFrom, EducationFrom
 from django.shortcuts import redirect
 # Create your views here.
 
@@ -23,43 +25,58 @@ def register(user_request):
         return render(user_request, 'collegeHub/registerTest.html',{'form':form})
     
 
+#@login_required
 def create_experience(user):
-    experiences = models.Experiences(user=user)
+    experiences = models.Experiences(profile=user)
     experiences.save()
 
 
+#@login_required
 def create_section(request):
     if request.method == 'POST':
-        data = json.loads(request.read().decode())
-        name = data['name']
-        experience_id = int(data['experience_id'])
+        form = SectionFrom(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.data.get('name')
+            form.save()
+            return JsonResponse({'name': name, 'fail': False}, status=200)
+        else:
+            return JsonResponse({'fail': True}, status=200)
+    else:
+        return
 
-        experience = get_object_or_404(models.Experiences, pk=experience_id)
-        section = models.Section(name=name, experiences=experience)
-        section.save()
 
-    return JsonResponse({'section_name':  name}, status=200)
-
-
+#@login_required
 def create_specific(request):
     if request.method == 'POST':
         form = SpecificForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            form.cleaned_data.get('image')
-            print(form.cleaned_data.get('description\n\n\n\n\n'))
-            form.cleaned_data.get('bullet_section')
-            form.cleaned_data.get('section')
         else:
             form = SpecificForm()
+    else:
+        form = SpecificForm()
 
-    return render(request, 'collegeHub/test.html', {'form': form})
+    return JsonResponse({}, status=200)
+
+
+#@login_required
+def create_education(request):
+    if request.method == 'POST':
+        form = SpecificForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        else:
+            form = EducationFrom()
+    else:
+        form = EducationFrom()
+    return JsonResponse({}, status=200)
 
 
 class Index(TemplateView):
     template_name = 'collegeHub/index.html'
 
 
+#@login_required
 class Settings(TemplateView):
     template_name = 'collegeHub/settings.html'
 
@@ -68,9 +85,20 @@ class test_page(TemplateView):
     template_name = 'collegeHub/test.html'
 
     def get(self, request):
-        form = SpecificForm(request.POST or None)
-        context = {'form': form}
+        secForm = SectionFrom(request.POST or None)
+        specForm = SpecificForm(request.POST or None)
+        eduForm = EducationFrom(request.POST or None)
+        context = {'sectionForm': secForm, 'specificForm': specForm, 'educationForm': eduForm}
         return render(request, 'collegeHub/test.html', context)
 
     def post(self, request):
-        return create_specific(request)
+        form = SectionFrom(request.POST, request.FILES)
+        if form.is_valid():
+            return create_section(request)
+        form = SpecificForm(request.POST, request.FILES)
+        if form.is_valid():
+            return create_specific(request)
+        form = EducationFrom(request.POST, request.FILES)
+        if form.is_valid():
+            return create_education(request)
+        return
