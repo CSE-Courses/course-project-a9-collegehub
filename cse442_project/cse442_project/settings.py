e0827d672e0caa1a0a68b9ecfc9e9305d0364632
MayankJ99 <mayanksjha@gmail.com>
2020-12-08 13:23:57 +0530

    hosting on heroku

--- cse442_project/cse442_project/settings.py
+++ cse442_project/cse442_project/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import psycopg2.extensions
-# import django_heroku
+import psycopg2.extensions
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'collegeHub/templates')
@@ -236,5 +236,6 @@ LOGGING = {
         }
     }
 }
-# django_heroku.settings(locals())
+DEBUG_PROPAGATE_EXCEPTIONS = True
+django_heroku.settings(locals())
 
